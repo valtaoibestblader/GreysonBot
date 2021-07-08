21f852f31648d5131a8d8dbb85b696c8935cc577
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-08 15:43:06 +0530

    Update __main__.py

--- GreysonBot/__main__.py
+++ GreysonBot/__main__.py
@@ -743,7 +743,7 @@ def main():
         LOGGER.info("Greyson Running ......")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
         updater.bot.send_message(
-            chat_id=SUPPORT_CHAT,
+            chat_id=MESSAGE_DUMP,
             text="I have been deployed successfully ...... Ready to run 🏃 ")
 
     if len(argv) not in (1, 3, 4):
