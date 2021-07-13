1254a5cca956d3a6efa364678756f10c078fe78f
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-13 22:03:59 +0530

    Update __main__.py

--- GreysonBot/__main__.py
+++ GreysonBot/__main__.py
@@ -408,7 +408,7 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🔙 Back", callback_data="help_back")]]
+                    [[InlineKeyboardButton(text="🔙 Backu", callback_data="help_back")]]
                 ),
             )
 
