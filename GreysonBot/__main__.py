e3b8de480444c36fada1eedf6179b9f5eff28bd1
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-13 22:12:41 +0530

    Added 2 btns

--- GreysonBot/__main__.py
+++ GreysonBot/__main__.py
@@ -408,7 +408,7 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🔙 Backu", callback_data="help_back")]]
+                    [[InlineKeyboardButton(text="Back",callback_data="help_back"),InlineKeyboardButton(text="Home",url="callback_data="bot_start")]]
                 ),
             )
 
