41409e6a1ab3b108692641a633862ee1e02eb28c
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-20 21:56:07 +0530

    Update __init__.py

--- GreysonBot/__init__.py
+++ GreysonBot/__init__.py
@@ -19,12 +19,6 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-log = logging.getLogger('[Enterprise]')
-logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
-log.info("[KIGYO] Kigyo is starting. | An Eagle Union Project. | Licensed under GPLv3.")
-log.info("[KIGYO] Not affiliated to Azur Lane or Yostar in any way whatsoever.")
-log.info("[KIGYO] Project maintained by: github.com/Dank-del (t.me/dank_as_fuck)")
-
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
