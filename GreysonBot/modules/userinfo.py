4a189d4ec5f60a02a61ce15105aa006e8aeeaf6b
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-27 22:23:40 +0530

    Opened Information

--- GreysonBot/modules/userinfo.py
+++ GreysonBot/modules/userinfo.py
@@ -515,6 +515,34 @@ def __user_info__(user_id):
     result = result.strip("\n")
     return result
 
+
+__help__ = """
+*Away from group*
+ ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+ ❍ brb <reason>*:* same as the afk command - but not a command.
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
+*ID:*
+ ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ ❍ /gifid*:* reply to a gif to me to tell you its file ID.
+*Self addded information:* 
+ ❍ /setme <text>*:* will set your info
+ ❍ /me*:* will get your or another user's info.
+*Examples:* 💡
+ ➩ /setme I am a wolf.
+ ➩ /me @username(defaults to yours if no user specified)
+*Information others add on you:* 
+ ❍ /bio*:* will get your or another user's bio. This cannot be set by yourself.
+ ❍ /setbio <text>*:* while replying, will save another user's bio 
+*Examples:* 💡
+ ➩ /bio @username(defaults to yours if not specified).`
+ ➩ /setbio This user is a wolf` (reply to the user)
+*Overall Information about you:*
+ ❍ /info*:* get information about a user. 
+ 
+*What is that health thingy?*
+ Come and see [HP System explained](https://t.me/GrupCinta)
+"""
+
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio)
 
@@ -535,6 +563,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
+__mod_name__ = "Bios/About"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
